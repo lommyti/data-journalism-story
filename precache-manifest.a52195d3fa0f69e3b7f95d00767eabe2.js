473caacfed7fbfc366e3f9092d63da3c17a2a1e0
Tommy Li <thomasli2020@u.northwestern.edu>
2019-03-13 15:13:11 -0700

    Updates

---
+++ precache-manifest.a52195d3fa0f69e3b7f95d00767eabe2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "497843656ef4a39696af",
+    "url": "/data-journalism-story/static/css/main.98058413.chunk.css"
+  },
+  {
+    "revision": "497843656ef4a39696af",
+    "url": "/data-journalism-story/static/js/main.94237b80.chunk.js"
+  },
+  {
+    "revision": "57aa5df115ec18e9ab26",
+    "url": "/data-journalism-story/static/js/runtime~main.1ca7abb7.js"
+  },
+  {
+    "revision": "c9e6ebe3a9dc848033bb",
+    "url": "/data-journalism-story/static/js/2.f75c2aa2.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/data-journalism-story/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "af2d02a1b0638428c6d5e16343792d6b",
+    "url": "/data-journalism-story/index.html"
+  }
+];
